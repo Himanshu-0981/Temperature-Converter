bdc18230c13de8b7cbbda9782f5c8ce365310977
Himanshu <kumarhimanshu2219@gmail.com>
2021-09-09 15:49:37 +0530

    second commit

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let cel = document.getElementById('cel');
 let fah = document.getElementById('fah');
+let resNum = document.getElementById('resultnum');
+
 
 cel.addEventListener('input',function(){
     let c = this.value;
     let f = (c * 9/5) + 32;
     fah.value = f;
+    resNum.innerHTML = `${f} F`;
 });
 
 
@@ -12,4 +15,6 @@ fah.addEventListener('input',function(){
     let f = this.value;
     let c = (f - 32) * 5/9
     cel.value = c;
+    resNum.innerHTML = `${c} C`;
 });
+
